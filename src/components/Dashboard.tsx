00a467790ff71de1f9c10bdc758af29f8531d1b0
fufangitgenesis <phu.pdnwork2701@gmail.com>
2025-08-01 16:09:30 +0700

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { KPICard } from "./KPICard";
 import { ActivityLogForm } from "./ActivityLogForm";
 import { VitalityChecklist } from "./VitalityChecklist";
