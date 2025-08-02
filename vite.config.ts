0b9ea4c8d771cd9d764f3c7a5b485ca0cd792d78
fufangitgenesis <phu.pdnwork2701@gmail.com>
2025-08-02 21:46:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "pki-ghpage" //allow vite project to be deployed on github
-}
+)
   server: {
     host: "::",
     port: 8080,
